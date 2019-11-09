42bcfc7096821f2c3e231cd8dd4a452724013805
Abhishek Kanparia <kanpariaabhishek@gmail.com>
2019-11-09 20:34:57 +0530

    Fix issue with routes not updating child after error

--- src/error/ErrorBoundaryRoute.js
+++ src/error/ErrorBoundaryRoute.js
@@ -3,14 +3,16 @@ import { Route } from "react-router-dom";
 import ErrorBoundary from "./ErrorBoundary";
 
 const ErrorBoundaryRoute = props => {
-  const { component, render, children, ...rest } = props;
+  const { component, render, children, errorBoundaryFor, ...rest } = props;
 
   if (!component && !render && !children) throw new Error(`No Component specified for Path - ${rest.path}`);
 
   const Component = component || render;
 
   const encloseInErrorBoundary = routerProps => (
-    <ErrorBoundary>{children ? children : <Component {...routerProps} />}</ErrorBoundary>
+    <ErrorBoundary {...(errorBoundaryFor ? { key: errorBoundaryFor } : {})}>
+      {children ? children : <Component {...routerProps} />}
+    </ErrorBoundary>
     /* Error within child will be caught if child is a component, else will be propogated up to nearest parent Errorboundary  */
   );
 
