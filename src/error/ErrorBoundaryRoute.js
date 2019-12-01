b2cea4c78b4e214bad290501f4cda1ca930d642c
Abhishek Kanparia <kanpariaabhishek@gmail.com>
2019-12-01 22:32:57 +0530

    Add wrapper component for 'children'

--- src/error/ErrorBoundaryRoute.js
+++ src/error/ErrorBoundaryRoute.js
@@ -5,18 +5,28 @@ import ErrorBoundary from "./ErrorBoundary";
 const ErrorBoundaryRoute = props => {
   const { component, render, children, errorBoundaryFor, ...rest } = props;
 
-  if (!component && !render && !children) throw new Error(`No Component specified for Path - ${rest.path}`);
+  if (!component && !render && !children)
+    throw new Error(`No Component specified for Path - ${rest.path}`);
 
   const Component = component || render;
 
   const encloseInErrorBoundary = routerProps => (
     <ErrorBoundary {...(errorBoundaryFor ? { key: errorBoundaryFor } : {})}>
-      {Component ? <Component {...routerProps} /> : children}
+      {Component ? (
+        <Component {...routerProps} />
+      ) : (
+        <ChildWrapper>{children}</ChildWrapper>
+      )}
     </ErrorBoundary>
-    /* Error within child will be caught if child is a component, else will be propogated up to nearest parent Errorboundary  */
+    /* Error within child will be caught if child is a component, else will be propogated up to nearest parent Errorboundary, 
+      Hence Wrapping it with a functional component  */
   );
 
   return <Route {...rest} render={encloseInErrorBoundary} />;
+};
+
+const ChildWrapper = ({ children }) => {
+  return <React.Fragment>{children}</React.Fragment>;
 };
 
 export default ErrorBoundaryRoute;
